1ed91e38957dd304aca058f79414f20eeceb00c0
Ashcon Mohseninia <ashconm@outlook.com>
2025-09-10 07:32:05 +0100

    Rustfmt

--- mcan/src/config.rs
+++ mcan/src/config.rs
@@ -68,14 +68,14 @@ pub struct BitTiming {
     /// MCAN peripheral is divisible into time quanta such that the bit time
     /// determined by `phase_seg_1` and `phase_seg_2` is a whole number of time
     /// quanta.
-    /// 
+    ///
     /// When [`allow_fractional`] is selected, the real bitrate
     /// is allowed to be up to 0.05% off (Which is within tolerance)
     pub bitrate: HertzU32,
     /// Allows for fractional bitrates where there may be no perfect
     /// combination of bit-timing parameters. This is useful for bitrates
     /// like 83.3kbps where some tolerance is allowed (Up to 0.05%).
-    pub allow_fractional: bool
+    pub allow_fractional: bool,
 }
 
 impl BitTiming {
@@ -91,7 +91,7 @@ impl BitTiming {
             phase_seg_1: 0xB,
             phase_seg_2: 0x4,
             bitrate,
-            allow_fractional: false
+            allow_fractional: false,
         }
     }
 }
